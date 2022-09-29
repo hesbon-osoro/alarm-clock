9899001aa8469aef2a330627b8a795908a940289
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-29 15:48:34 +0300

    mousemove event

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Alarm from './context/Alarm';
 import AlarmOption from './components/AlarmOption';
 import DigitalClock from './components/DigitalClock';
 import './App.css';
+import ProximitySensor, { onMouseMove } from './components/ProximitySensor';
 
 function App() {
   return (
-    <section className="clock container">
+    <section className="clock container" onMouseMove={onMouseMove}>
+      <ProximitySensor />
       <div className="clock-container grid">
         <div className="clock-content grid">
           <Alarm>
